6d642ebafb20c251df1762ac5ac1e6680784db44
Lyle Kilson <lylekilsongithub@gmail.com>
2021-07-17 20:27:22 -0600

    test

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
 
