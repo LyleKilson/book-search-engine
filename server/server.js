7290648f205af1eafcbf58c9f623548da9db5165
Lyle Kilson <lylekilsongithub@gmail.com>
2021-07-17 18:55:32 -0600

    server updated along with uninstalling unnecessary NPM's

--- server/server.js
+++ server/server.js
@@ -1,25 +1,19 @@
 const express = require("express");
 const path = require("path");
-// importing the ApolloServer
-const { ApolloServer } = require("apollo-server-express");
-
 const db = require("./config/connection");
-const routes = require("./routes");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
+//const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const { typeDefs, resolvers } = require("./schema");
-
-// create a new apollo server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
-// integrate apollo server with express app as middleware
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
@@ -33,8 +27,6 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
-
-app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
